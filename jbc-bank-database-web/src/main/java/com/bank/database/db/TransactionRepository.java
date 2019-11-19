2cf6a9672d0c0686909a732478c20dc69d17ad86
Fred Wills <willsf422@gmail.com>
2019-11-19 02:57:45 -0500

    deleted outdated jbc bank database

--- jbc-bank-database-web/src/main/java/com/bank/database/db/TransactionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.bank.database.db;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.bank.database.business.Transaction;
-
-public interface TransactionRepository extends CrudRepository<Transaction, Integer> {
-
-}
